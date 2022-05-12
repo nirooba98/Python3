174ada62ad41136bf66d8cc38d5a703f24296a66
Pon Nirooba <90277335+nirooba98@users.noreply.github.com>
2022-05-12 23:03:05 +0530

    Update escapeCharacters.py

--- PythonStrings/escapeCharacters.py
+++ PythonStrings/escapeCharacters.py
@@ -1,9 +1,11 @@
 new_line = "You can create a new line \nwithin a string by using escape characters"
 quotes = "You cannot use \"quotation\" marks within a string without a backslash"
 slash = "You can print any character by using a \\ backslash in the front"
+
 """
 There are many more escape characters in python. You can check them out in websites like w3schools.
 """
+
 print(new_line)
 print(quotes)
 print(slash)
