764bf6e2bcfe4570cd518ed665462c861200727b
Pon Nirooba <90277335+nirooba98@users.noreply.github.com>
2022-05-15 19:24:17 +0530

    Add files via upload

---
+++ python_user_input/get_input.py
@@ -0,0 +1,9 @@
+print("you can get input from user in python by using the input function")
+print("Please enter your name:")
+name = input()           # the value entered by the user can be stored in a variable
+print("Welcome " + name + "!")       # and can be used in a print statement
+
+
+print("The input function has a \'prompt\' parameter. Any string value is passed as the parameter")
+age = input("Enter your age: ")
+print("You are " + age + " years old!")
---
+++ python_user_input/input_numbers.py
@@ -0,0 +1,9 @@
+num1 = input("Enter any number: ")
+num2 = input("Enter another number: ")
+result = float(num1) + float(num2)
+print(result)
+"""
+Python considers any input value to be a string. 
+We can use either an "int" or a "float" function to convert the datatype.
+Python will convert the input datatype before performing the calculation.
+"""
