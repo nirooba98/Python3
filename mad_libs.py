5c7729e7501eca167ab414630d0032b0100358c9
Pon Nirooba <90277335+nirooba98@users.noreply.github.com>
2022-05-16 19:05:23 +0530

    Add files via upload

---
+++ mad_libs.py
@@ -0,0 +1,23 @@
+adjective1 = input("Enter an adjective: ")
+noun1 = input("Enter a noun: ")
+adjective2 = input("Enter an adjective: ")
+noun2 = input("Enter a noun: ")
+adjective3 = input("Enter an adjective: ")
+adjective4 = input("Enter an adjective: ")
+plural_noun = input("Enter a plural noun: ")
+noun3 = input("Enter a noun: ")
+number1 = input("Enter a number: ")
+shapes = input("Enter a shape: ")
+food1 = input("Enter a food: ")
+food2 = input("Enter a food: ")
+number2 = input("Enter a number: ")
+
+print("To make the/a " + adjective1 + " Pizza, you need to take a lump of " + noun1 + " , "
+      "and make a thin, round " + adjective2 + " " + noun2 + ".")
+print("Then you cover it with " + adjective3 + " sauce, " + adjective4 + " cheese, "
+      "and fresh chopped " + plural_noun + ".")
+print("Next you have to bake it in a very hot " + noun3 + ". "
+      "When it is done, cut it into " + number1 + " " + shapes + ".")
+print("Some people like " + food1 + " pizza the best, "
+      "but my favorite is the " + food2 + " pizza.")
+print("If I could, I would eat pizza " + number2 + " times a day ")
