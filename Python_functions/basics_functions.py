04e28ec3f14fd296578430b45b48eb4ed1aaa7dd
Pon Nirooba <90277335+nirooba98@users.noreply.github.com>
2022-05-23 19:58:18 +0530

    Add files via upload

---
+++ Python_functions/basics_functions.py
@@ -0,0 +1,30 @@
+def welcome():
+    print("Welcome, Let's learn Python")
+
+
+welcome()
+
+
+def add(x, y):
+    print("The added answer is:")
+    print(x + y)
+
+
+add(10, 10)
+
+
+def multiply(x, y):
+    return x * y
+
+
+print("The multiplied value is:")
+print(multiply(4, 3))
+
+
+def power(y):
+    result = y * y * y
+    return result
+
+
+print("The result is:")
+print(power(2))
