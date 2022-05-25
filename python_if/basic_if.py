1fc7214c3f10c6b92d4f93c3340d61bff1d4abd6
Pon Nirooba <90277335+nirooba98@users.noreply.github.com>
2022-05-25 20:37:28 +0530

    Add files via upload

---
+++ python_if/basic_if.py
@@ -0,0 +1,16 @@
+print("Wear the perfect outfit for the appropriate season")
+season = input("Type in a Season: ")
+if season == "Summer":
+    print("Perfect time to wear a white or a yellow outfit! Light blue is my fav!!")
+elif season == "Spring":
+    print("Wear something pink or yellow or light green!")
+elif season == "Fall":
+    print("perfect time to wear something orange or olive!! And halloween is on its way!")
+else:
+    print("Winter is here! Wear black, tan or something in Red. Merry Christmas you all!")
+
+"""
+An if statement is used to check if a particular condition is either true or false.
+we use an else keyword in python to specify an action if the condition is false.
+we can specify multiple actions for different conditions using if.. elif.. else..
+"""
---
+++ python_if/compare_op.py
@@ -0,0 +1,12 @@
+print("Find the greatest number")
+a = input("Type in the first number: ")
+b = input("Type in the second number: ")
+if a > b:
+    print(str(a) + " is the greatest number")
+elif a == b:
+    print("Both are equal numbers.")
+else:
+    print(str(b) + " is the greatest number")
+"""
+By using the if statements we can compare numbers and string with each other. 
+"""
