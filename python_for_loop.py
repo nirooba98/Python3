a4124b8c834f83d78abd43d7a1c37b2844e5f5e0
Pon Nirooba <90277335+nirooba98@users.noreply.github.com>
2022-06-03 21:42:14 +0530

    Add files via upload

---
+++ python_for_loop.py
@@ -0,0 +1,30 @@
+
+"""
+Syntax:
+for variable in sequence:
+    print("instructions")
+"""
+
+for char in "shiva":        # looping through a string. Prints each letter of the string in a new line
+    print(char)
+
+
+colours = ["orange", "Yellow", "olive", "black"]
+for colour in colours:      # Prints all the values in an array.
+    print(colour)
+
+
+for i in range(6, 15):      # prints the numbers (0 - 14)
+    print(i)
+
+
+for shade in colours:
+    if shade == "olive":    # if statement breaks the loop to execute a specified condition.
+        print("olive - Colour of the month!")
+    else:
+        print(shade)        # if condition untrue, prints the values in the array.
+
+clothes = ["Shirt", "Gown", "Jacket"]
+for dress in clothes:
+    for color in colours:
+        print(color, dress)
