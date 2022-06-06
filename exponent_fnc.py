9f80a8a4183010ffcc79a7838fb5475e9d6999a9
Pon Nirooba <90277335+nirooba98@users.noreply.github.com>
2022-06-06 22:04:50 +0530

    Update exponent_fnc.py

--- exponent_fnc.py
+++ exponent_fnc.py
@@ -1,8 +1,8 @@
 
-def find_exponent_result(num1, num2):
+def find_exponent_result(num1, num2):           # defining a function to calculate exponent value. Here, (num1 = base value) and (num2 = power value)
     answer = 1
-    for i in range(num2):
-        answer = answer * num1
+    for i in range(num2):                       # we can write a for loop with the power value as the range.
+        answer = answer * num1                  # the base value is multiplied with itself until the loop ends.
     return answer
 
 
